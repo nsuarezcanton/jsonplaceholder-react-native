27e2b1ee7a1da3adb2aeaad788d52b9416e45298
Nico Suarez-Canton <nsuarez.canton@gmail.com>
2018-10-21 16:03:52 +0200

    Updated `AlbumList`styles

--- src/components/AlbumList/index.js
+++ src/components/AlbumList/index.js
@@ -4,59 +4,6 @@ import PropTypes from 'prop-types';
 import AlbumListCell from './AlbumListCell';
 import { BaseStyles } from '../../styles';
 
-const TEST_DATA = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'quidem molestiae enim',
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: 'sunt qui excepturi placeat culpa',
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: 'omnis laborum odio',
-  },
-  {
-    userId: 1,
-    id: 4,
-    title: 'non esse culpa molestiae omnis sed optio',
-  },
-  {
-    userId: 1,
-    id: 5,
-    title: 'eaque aut omnis a',
-  },
-  {
-    userId: 1,
-    id: 6,
-    title: 'natus impedit quibusdam illo est',
-  },
-  {
-    userId: 1,
-    id: 7,
-    title: 'quibusdam autem aliquid et et quia',
-  },
-  {
-    userId: 1,
-    id: 8,
-    title: 'qui fuga est a eum',
-  },
-  {
-    userId: 1,
-    id: 9,
-    title: 'saepe unde necessitatibus rem',
-  },
-  {
-    userId: 1,
-    id: 10,
-    title: 'distinctio laborum qui',
-  },
-];
-
 const styles = StyleSheet.create({
   contentContainer: { padding: BaseStyles.SPACING_BASE },
   separator: {
@@ -68,7 +15,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const AlbumList = ({ albumList = TEST_DATA, onTapItem }) => (
+const AlbumList = ({ albumList, onTapItem }) => (
   <FlatList
     contentContainerStyle={[styles.contentContainer]}
     data={albumList}
